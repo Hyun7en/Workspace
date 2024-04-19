71266735fe3ddc8ffc85d741cd790f23999bb041
alwaysJOne <alwaysjone@naver.com>
2024-04-19 09:40:44 +0900

    .

--- 8_spring/spring/src/main/java/com/kh/spring/board/controller/BoardController.java
+++ 8_spring/spring/src/main/java/com/kh/spring/board/controller/BoardController.java
@@ -1,11 +1,17 @@
 package com.kh.spring.board.controller;
 
+import java.util.ArrayList;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.kh.spring.board.model.vo.Board;
 import com.kh.spring.board.service.BoardService;
+import com.kh.spring.common.model.vo.PageInfo;
+import com.kh.spring.common.template.Pagination;
 
 @Controller
 public class BoardController {
@@ -14,9 +20,14 @@ public class BoardController {
 	private BoardService boardService;
 	
 	@RequestMapping("list.bo")
-	public String selectList(@RequestParam(value="cpage", defaultValue="1") int currentPage) {
+	public String selectList(@RequestParam(value="cpage", defaultValue="1") int currentPage, Model model) {
 		int boardCount = boardService.selectListCount();
-		System.out.println(boardCount);
+		
+		PageInfo pi = Pagination.getPageInfo(boardCount, currentPage, 10, 5);
+		ArrayList<Board> list = boardService.selectList(pi);
+		
+		model.addAttribute("list", list);
+		model.addAttribute("pi", pi);
 		
 		return "board/boardListView";
 	}
