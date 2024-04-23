3ad6bf2178b1889489380745ac1da99d36cd4823
alwaysJOne <alwaysjone@naver.com>
2024-04-23 09:50:42 +0900

    .

--- 8_spring/spring/src/main/java/com/kh/spring/board/controller/BoardController.java
+++ 8_spring/spring/src/main/java/com/kh/spring/board/controller/BoardController.java
@@ -1,6 +1,8 @@
 package com.kh.spring.board.controller;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -8,6 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.google.gson.Gson;
 import com.kh.spring.board.model.vo.Board;
@@ -58,5 +61,30 @@ public class BoardController {
 		ArrayList<Reply> list = boardService.selectReply(bno);
 		
 		return new Gson().toJson(list);
+	}
+	
+	@RequestMapping("enrollForm.bo")
+	public String enrollForm() {
+		return "board/boardEnrollForm";
+	}
+	
+	@RequestMapping("insert.bo")
+	public String insertBoard(Board b, MultipartFile upfile) {
+		System.out.println(b);
+		System.out.println(upfile);
+		
+		//전달된 파일이 있을 경우 => 파일이름 변경 => 서버에 저장 => 원본명, 서버업로드된 경로를 b객체에 담기
+		if(!upfile.getOriginalFilename().equals("")) {
+			String changeName = saveFile(upfile);
+			
+		}
+		return "main";
+	}
+	
+	//실제 넘어온 파일의 이름을 변경해서 서버에 저장하는 메소드
+	public String saveFile(MultipartFile upfile) {
+		String originName = upfile.getOriginalFilename();
+		
+		String currentTime = new SimpleDateFormat("yyyyMMddHHmmss").format(new Date());
 	}
 }
