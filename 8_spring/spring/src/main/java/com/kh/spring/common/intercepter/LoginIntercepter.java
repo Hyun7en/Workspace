c5448e5e609dd431e3488f80563543ea6605f924
alwaysJOne <alwaysjone@naver.com>
2024-04-24 12:34:48 +0900

    .

--- 8_spring/spring/src/main/java/com/kh/spring/common/intercepter/LoginIntercepter.java
+++ 8_spring/spring/src/main/java/com/kh/spring/common/intercepter/LoginIntercepter.java
@@ -4,9 +4,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+import org.springframework.web.servlet.HandlerInterceptor;
 
-public class LoginIntercepter extends HandlerInterceptorAdapter{
+public class LoginIntercepter implements HandlerInterceptor{
 
 	/*
 	 * HandlerInterceptor
