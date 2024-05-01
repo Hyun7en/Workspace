be89504d0fa24c8aa8ef0fd2bb1908c8e5c80e77
alwaysJOne <alwaysjone@naver.com>
2024-05-01 15:09:47 +0900

    .

--- 9_react/react-app/src/App.js
+++ 9_react/react-app/src/App.js
@@ -4,12 +4,14 @@ import Menu from './components/Menu';
 import Comment from './components/Comment';
 import CommentList from './classComponent/CommentList';
 import UseStateTest from './reactHook/UseStateTest';
+import SignUp from './sample/SignUp';
+import LandingPage from './sample/LandingPage';
 
 
 function App() {
   return (
     <div className="App">
-      <UseStateTest />
+      <LandingPage />
     </div>
   );
 }
