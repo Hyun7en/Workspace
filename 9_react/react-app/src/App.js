13c8599b1a7f4c2d03ccd8ebb9682e88d06b68ea
alwaysJOne <alwaysjone@naver.com>
2024-05-02 14:32:36 +0900

    .

--- 9_react/react-app/src/App.js
+++ 9_react/react-app/src/App.js
@@ -10,13 +10,40 @@ import UseEffectTest from './reactHook/UseEffectTest';
 import UseMemoTest from './reactHook/useMemo/useMemoTest';
 import UseCallbackTest2 from './reactHook/useCallback/UseCallbackTest2';
 import UseRefTest from './reactHook/UseRefTest';
+import {BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Home from './pages/Home';
+import FoodMenu from './pages/FoodMenu';
+
+/*
+  react-router-dom
+  react로 생성된 SPA 내부에서 페이지 이동이 가능하도록 만들어주는 라이브러리
+
+  BrowserRouter(Router)
+  history API를 이용해서 history객체를 생성한다.
+  라우팅을 진행할 컴포넌트 상위에 Router컴포넌트를 생성하고 감싸주어야한다.
+*/
 
 
 function App() {
   return (
-    <div className="App">
-      <UseRefTest/>
-    </div>
+    <Router>
+      <div className="App">
+        <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/food">Food List</Link>
+              </li>
+            </ul>
+        </nav>
+       <Routes>
+          <Route path='/' element={<Home />}/>
+          <Route path='/food' element={<FoodMenu />}/>
+       </Routes>
+      </div>
+    </Router>
   );
 }
 
