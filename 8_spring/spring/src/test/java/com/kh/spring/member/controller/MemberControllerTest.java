c31dd3674070e277edd85043b4a7b9fd0ab874b7
alwaysJOne <alwaysjone@naver.com>
2024-06-13 14:37:56 +0900

    .

--- 8_spring/spring/src/test/java/com/kh/spring/member/controller/MemberControllerTest.java
+++ 8_spring/spring/src/test/java/com/kh/spring/member/controller/MemberControllerTest.java
@@ -1,7 +1,10 @@
 package com.kh.spring.member.controller;
 
+import static org.junit.Assert.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.Before;
@@ -12,6 +15,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
@@ -47,7 +51,28 @@ public class MemberControllerTest {
 					.param("userId", "admin") // 요청 파라미터 userId 세팅
 					.param("userPwd", "1234"))
 			.andDo(print()) // 요청한거에대한 응답코드를 콘설에 출력해줘
-			.andExpect(status().isOk()); // HTTP 상태코드가 200ok인지 확인해줘
+			.andExpect(status().isFound())// HTTP 상태코드가 302인지 확인해줘
+			.andExpect(redirectedUrl("/"));  // 리다이렉트 경로 확인해줘
+//			.andExpect(status().isOk()); // HTTP 상태코드가 200ok인지 확인해줘
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void testIdCheck() {
+	    // MockMvc를 사용하여 GET 요청을 보냄
+        MvcResult result;
+		try {
+			result = mockMvc.perform(
+					get("/idCheck.me")
+			        .param("checkId", "jiwon0909")) // checkId 파라미터 설정
+			        .andExpect(status().isOk())
+			        .andReturn();
+
+	        // 응답 본문의 내용 확인 
+	        String content = result.getResponse().getContentAsString();
+	        assertEquals("NNNNY", content); // 아이디 사용가능 결과와 비교
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
