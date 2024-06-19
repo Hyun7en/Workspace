af26d2844fd6faee93371b5c8cebb9967b185f11
alwaysJOne <alwaysjone@naver.com>
2024-06-19 16:58:37 +0900

    .

--- 10_springboot/boot/src/main/java/com/kh/boot/controller/BootController.java
+++ 10_springboot/boot/src/main/java/com/kh/boot/controller/BootController.java
@@ -1,27 +1,42 @@
 package com.kh.boot.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
-@RestController
+@RestController("/board")
 public class BootController {
 
-    @GetMapping("/dummy")
-    public String dummy(){
-      log.info("dummy성공");
-      return "ok";
+//    @GetMapping("/dummy")
+//    public String dummy(){
+//      log.info("dummy성공");
+//      return "ok";
+//    }
+//
+//    @GetMapping("/member")
+//    public String getMember(@RequestParam(value = "memberKey", defaultValue = "111") String memberKey,
+//                            @RequestParam(value = "age") int age){
+//        log.info("memberKey : {}", memberKey);
+//        log.info("age : {}", age);
+//        return "ok";
+//    }
+
+    //ResponseEntity : Http응답을 나타내는 클래스이다.
+    @PostMapping
+    public ResponseEntity<Boolean> create(){
+
+        //Board객체를 생성하기위함
+        // userId, pwd, title, contents
+
+
+        return new ResponseEntity<>(true, HttpStatus.OK);
     }
 
-    @GetMapping("/member")
-    public String getMember(@RequestParam(value = "memberKey", defaultValue = "111") String memberKey,
-                            @RequestParam(value = "age") int age){
-        log.info("memberKey : {}", memberKey);
-        log.info("age : {}", age);
-        return "ok";
+    @GetMapping
+    public void getBoardList(){
+
     }
 }
